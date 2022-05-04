429e852c97afb8351d971cb0e7872bd3326e3b3e
bjiake <suyd@list.ru>
2022-05-04 11:10:17 +0300

    Добавьте файлы проекта.

---
+++ Server/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketTcpServer
+{
+    class Program
+    {
+        static int port = 2115; // порт для приема входящих запросов
+        static string HostName = "127.0.0.1";// Порт сервера 176.196.126.194 
+        //Порт локальной 127.0.0.1
+
+        public static void RecievePlayerData(Socket Player)
+        {
+            string message = "Ждём игроков";
+            StringBuilder builder = new StringBuilder();
+            int bytes = 0; // количество полученных байтов
+            byte[] data = new byte[256]; // буфер для получаемых данных
+            do
+            {
+                bytes = Player.Receive(data);
+                builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+            }
+            while (Player.Available > 0);
+
+            Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
+
+            // отправляем ответ
+            message = "ваше сообщение доставлено";
+            data = Encoding.Unicode.GetBytes(message);
+            Player.Send(data);
+            data = null;
+            bytes = 0;
+            builder.Clear();
+        }
+        
+        public static void SendPlayerData(string message, Socket Player, Socket PlayerTwo)
+        {
+            StringBuilder builder = new StringBuilder();
+            byte[] data = new byte[256]; // буфер для получаемых данных
+
+            data = Encoding.Unicode.GetBytes(message);
+            Player.Send(data);
+            PlayerTwo.Send(data);
+            builder.Clear();
+            data = null;
+        }
+
+        public static void SendAlonePlayerData(string message, Socket Player)
+        {
+            StringBuilder builder = new StringBuilder();
+            byte[] data = new byte[256]; // буфер для получаемых данных
+
+            data = Encoding.Unicode.GetBytes(message);
+            Player.Send(data);
+            builder.Clear();
+            data = null;
+        }
+        static void Main(string[] args)
+        {
+            Console.Title = "Блэйк Джек";
+
+            ConsoleColor[] colors = (ConsoleColor[])ConsoleColor.GetValues(typeof(ConsoleColor));
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.Clear();
+
+            Console.BufferWidth = 120;
+            Console.WindowWidth = Console.BufferWidth;
+            Console.WindowHeight = 40;
+            Console.BufferHeight = Console.WindowHeight;
+
+            
+
+            
+
+
+
+
+            // получаем адреса для запуска сокета
+            var IpPoint = new IPEndPoint(IPAddress.Parse(HostName), port);
+
+            // создаем сокет
+            var listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                // связываем сокет с локальной точкой, по которой будем принимать данные
+                listenSocket.Bind(IpPoint);
+
+                // начинаем прослушивание очередь входящих потоков
+                listenSocket.Listen(5);
+
+                Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+                while (true)
+                {
+                    string message = "Ждём игроков\n";
+
+
+                    Socket Player = listenSocket.Accept();
+
+                    message = "Игрок 1 подключился\n";
+                    SendAlonePlayerData(message, Player);
+                    Socket PlayerTwo = listenSocket.Accept();
+
+                    SendPlayerData(message, Player, PlayerTwo);
+                    message = "Игрок 2 подключился\n";
+                    SendPlayerData(message, Player, PlayerTwo);
+
+                    DealCards DealCard = new();
+                    DealCard.Deal();
+                    //Console.ReadLine();
+
+                    // получаем сообщение
+                    RecievePlayerData(Player);
+                    RecievePlayerData(PlayerTwo);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
